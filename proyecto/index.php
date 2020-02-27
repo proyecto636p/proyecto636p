cc4d8b5044a7945f22e65d5e773405c771697eca
programador1 <proyecto636p@gmail.com>
2020-02-27 14:13:47 -0400

    subida del proyecto a github

---
+++ proyecto/index.php
@@ -0,0 +1,170 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inces Regional Portuguesa. División de Informática</title>
+	<!-- <link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css"> -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/css.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="materialize/css/material-icons.css">
+</head>
+<body>
+	<div class="menu">
+		<img src="imagenes/cintillos.jpg" style="height: 45px;" width="100%">
+		
+	</div>
+	<nav class="sa navbar-dark" style="height: 40px;">
+		<div class="py-1" style="width: 1200px; position: absolute;">
+		<marquee align=center direction=""><font color="#FFFFFF"><font size=4>Fecha Hora entre otros</font> </font> </marquee>
+		</div>
+		<ul class="nav justify-content-end">
+		  <li class="nav-item">
+		  <!-- 	<a class="nav-link active icon-facebook" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true"></a> -->
+		    <a class="navbar-brand" href="#"><i class="icon-facebook"></i></a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="navbar-brand" href="#"><i class="icon-twitter"></i></a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="navbar-brand" href="#"><i class="icon-instagram"></i></a>
+		  </li>
+		</ul>
+	</nav>
+	<div class="carusel">
+		<div class="intra">
+			<div class="contain">
+			<div class="texter" style="height: 40px;">
+				<h4 class="text-light py-2">INTRANET</h4>
+			</div>
+			<div class="pp py-2">
+				<!-- <button type="button" class="btn btn-light btn-sm btn-block font-weight-bolder text-secondary"><i class="icon-twitter"></i>IntraInces PNA</button> -->
+				<a href="#" class="btn btn-primary" role="button">IntraInces Pna</a>
+			</div>
+			<div class="pp py-1">
+				 <!-- 
+				 	<button type="button" class="btn  btn-light btn-sm btn-block text-secondary font-weight-bolder" href="#"><i class="icon-twitter"></i> IntraInces Informatica</button>
+			
+				  -->
+				 <a href="intra.php" class="btn btn-danger" role="button">IntraInces Informatica</a>
+			</div>
+		</div>
+		</div>
+		<div class="text-center bg-light" style="position: absolute; background:rgba(0,0,0,.5,#F2F2F2); width: 18rem;">
+			<div>
+				<button type="button" class="btn btn-secondary btn-sm">Small button</button>
+			</div>
+			<div>
+				<button type="button" class="btn btn-secondary btn-sm">Small button</button>
+			</div>
+		</div>
+		<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		        <img src="./imagenes/Inces2.jpg" style="height: 280px;" class="d-block w-100" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="./imagenes/inces1.jpg" style="height: 280px;" class="d-block w-100" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="./imagenes/1.jph" style="height: 280px;" class="d-block w-100" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="./imagenes/misionvision.png" style="height: 280px;" class="d-block w-100" alt="...">
+		    </div>
+		  </div>
+	</div>
+	</div>
+	<div class="container text-center" style="height:auto;">
+			<h3 class="text-gray">Servicios Asociados</h3>
+		<div class="py-4">
+			<table class="table table-borderless">
+		      <thead>
+		        <tr>
+		        	<th scope="col">
+		          		<div class="card" style="width: 15rem;">
+					   <div class="panel-group">
+						    <div class="panel panel-primary">
+						      <div class="panel-heading">PNA</div>
+						      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						      consequat. </div>
+						    </div>
+						</div>
+						</div>
+		          </th>
+
+		          <th scope="col">
+		          	<div class="card" style="width: 15rem;">
+					  <img src="imagenes/pna.jpg" style="height: 180px;" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <p class="card-text">Link Asociado 1</p>
+					  </div>
+					</div>
+		          </th>
+		          <th scope="col">
+		          	<div class="card" style="width: 15rem;">
+					  <img src="imagenes/soporte.png" style="height: 180px;" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <p class="card-text">Link Asociado 2</p>
+					  </div>
+					</div>
+		          </th>
+		          	<th scope="col">
+		          		<div class="card" style="width: 15rem;">
+					   <div class="panel-group">
+						    <div class="panel panel-primary">
+						      <div class="panel-heading">INFORMATICA</div>
+						      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						      consequat. </div>
+						    </div>
+						</div>
+						</div>
+		          </th>
+		       
+		        </tr>
+		      </thead>
+		    </table>
+		</div>
+		</div>
+
+		<!-- <div class="container text-center">
+			<h3 class="text-gray">Enlaces INCES</h3>
+			<div class="py-4">
+			<table class="table table-borderless">
+		      <thead>
+		        <tr>
+		          <th class="ia">
+					<a class="btn" href=""><i class="material-icons">navigation</i>Pagina Oficial</a>
+		          </th>
+		          <th class="ia">
+					<a class="btn" href="http://www.vicepresidencia.gob.ve/tag/inces/"><i class="material-icons">low_priority</i>INCES Portal-VP</a>
+		          </th>
+		          <th class="ia">
+					<a class="btn" href="https://inces.gob.ve/"><i class="material-icons">domain</i>Pagina Oficial</a>
+		          </th>
+		        </tr>
+		      </thead>
+		    </table>
+		</div>
+		</div>
+		-->
+		<div class="cons">
+			<div class="media">
+			  <img src="imagenes/logo.png" class="align-self-center mr-3" alt="...">
+			  <div class="media-body">
+			    <h6 class="mt-1 text-center">Dirección: Av. Principal Urb. Baraure 1 detrás del terminal de pasajeros de Acarigua-Araure Teléfonos: 0255-6216167/6227667</h6>
+			  </div>
+			</div>
+		</div>
+
+
+ 
+
+  <script type="text/javascript" src="jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
